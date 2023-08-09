4880036cf243027b681ac8e75372381885c1299f
Douglas Vinicius <vinibonin007@gmail.com>
2023-08-08 23:39:54 -0300

    Feat: Add function examples.

--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,18 @@
-puts 'Hello world'
+# Function examples
+def printMessage(message)
+    puts "Message: #{message}"
+end
+
+def sum(n1, n2)
+    total = n1 + n2
+    puts "#{n1} + #{n2} =  #{total}"
+end
+
+
+printMessage('Hello world from function')
+printMessage 'Hello world without ()'
+
+sum(2,2)
+sum 3,4
+
+
